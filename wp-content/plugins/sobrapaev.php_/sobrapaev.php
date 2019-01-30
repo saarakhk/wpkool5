5f570705537c9f182386f9eea28a2ffd1516acd3
saara.allik <saara.vallik@khk.ee>
2019-01-30 22:07:21 +0200

    installed plugin `Sobrapaev` version 1.0

---
+++ wp-content/plugins/sobrapaev.php_/sobrapaev.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin name: Sobrapaev
+ * Plugin URI: http://saaravallik.ikt.khk.ee/
+ * Description: This is the very first plugin I ever created
+ * Version: 1.0
+ * Author: Saara V채llik
+ * Author URI: http://saaravallik.ikt.khk.ee/
+ * */
+ 
+ ?>
+ 
+<script src="//code.jquery.com/jquery.min.js"></script>
+<script src="jquery.flurry.js"></script>
+
+$('body').flurry();
